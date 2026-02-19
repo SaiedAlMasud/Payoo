2a7bd29f5ed45db415ff77c29fb030bb523e597e
Ankon 190 <ankon190@gmail.com>
2026-02-19 16:02:58 +0600

    login validation added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,20 @@
-//console.log("Login script loaded");
+console.log("Login script loaded");
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    console.log("Login button clicked");
+    const mobileNumber = document.getElementById("mobile-number").value;
+    const password = document.getElementById("password").value;
+    if (mobileNumber && password) {
+        if(mobileNumber === "1234567890" && password === "1234") {
+            alert("Login successful!"); 
+            window.location.assign("pages/home.html");
+        }
+        else {
+            alert("Invalid mobile number or password.");
+            return;
+        }
+    }
+    else {
+        alert("Please enter both mobile number and pin.");
+    }
+});
