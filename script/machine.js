e52ec536cf592b7c7b2e6ebce4748218ad9ecf30
Ankon 190 <ankon190@gmail.com>
2026-02-20 15:43:26 +0600

    add money and cashout button click and from appear function complete

--- script/machine.js
+++ script/machine.js
@@ -3,3 +3,15 @@ function getValueFromId(id) {
     const value = document.getElementById(id).value;
     return value;
 }
+
+function showOnly(id){
+    const addMoney = document.getElementById("add-money-section");
+    const cashout = document.getElementById("cashout-section");
+   
+    addMoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+
+    //show the selected section
+    const sectionToShow = document.getElementById(id);
+    sectionToShow.classList.remove("hidden");
+}
